0f64c3f540bdf285279746fd06aa274e04c1e594
harshithkrpt <harshith.krpt@gmail.com>
2020-02-23 11:59:22 +0530

    note_app_final

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,62 @@
-const utils = require("./utils");
+const yargs = require("yargs");
+const notes = require("./notes");
 
-console.log(utils.add(2, 3));
+// Customize add commands
+yargs.command({
+  command: "add",
+  describe: "Add a new Note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string"
+    },
+    body: {
+      describe: "Note Body",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: argv => {
+    notes.addNote(argv.title, argv.body);
+  }
+});
+
+yargs.command({
+  command: "remove",
+  describe: "remove a new Note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      type: "string",
+      demandOption: true
+    }
+  },
+  handler: argv => {
+    notes.removeNote(argv.title);
+  }
+});
+
+yargs.command({
+  command: "read",
+  describe: "read a new Note",
+  builder: {
+    title: {
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: argv => {
+    notes.readNote(argv.title);
+  }
+});
+
+yargs.command({
+  command: "list",
+  describe: "list a new Note",
+  handler: () => {
+    notes.listNotes();
+  }
+});
+
+yargs.parse();
--- notes-app/utils.js
+++
@@ -1,7 +0,0 @@
-const name = "Harshith";
-
-const add = (a, b) => {
-  return a + b;
-};
-
-module.exports = { name, add };

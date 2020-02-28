be1c37b8b4e5ed18bd9ff0342ba112c749d55fde
harshithkrpt <harshith.krpt@gmail.com>
2020-02-28 21:47:23 +0530

    Chat Application Not Implemented

--- chat-app/public/js/chat.js
+++
@@ -1,9 +0,0 @@
-const socket = io();
-
-socket.on("countUpdated", count => {
-  console.log("The Count has been Updated", count);
-});
-
-document.querySelector("#increment").addEventListener("click", e => {
-  socket.emit("increment");
-});

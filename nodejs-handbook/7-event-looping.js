93843cbdc9fdcc9537cfb2b7ed69146b2cc7fe64
harshithkrpt <harshith.krpt@gmail.com>
2020-03-17 10:22:02 +0530

    minor updates of packages

--- nodejs-handbook/7-event-looping.js
+++ nodejs-handbook/7-event-looping.js
@@ -1,3 +1,30 @@
 setImmediate(() => {
-  console.log("hELLO");
+  console.log("hELLO Set Immediate");
 });
+
+process.nextTick(() => {
+  console.log("hLLO Next Tick");
+});
+
+new Promise((resolve, reject) => resolve("Hello World Promise")).then(resolve =>
+  console.log(resolve)
+);
+
+const myFunction = (first, second) => {
+  console.log(first, second);
+};
+
+setTimeout(myFunction, 1000, "hL", "Jl");
+
+const id = setTimeout(() => {
+  // should run after 2 seconds
+}, 2000);
+// I changed my mind
+clearTimeout(id);
+
+const is = setInterval(() => {
+  console.log("Hell");
+  // runs every 2 seconds
+}, 2000);
+
+clearInterval(is);
